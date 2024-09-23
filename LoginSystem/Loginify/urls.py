82ae15aeb8c63f1399eaa0aff8d39a872194ec27
malyaj93 <m.sirothia93@gmail.com>
2024-09-22 20:36:07 -0700

    Task 3 done
    1. Create a UserDetail model with username, email & password field
    2. define URLs and templates
    3. write signup & login view logic to include the functionality of login and signup

--- LoginSystem/Loginify/urls.py
+++ LoginSystem/Loginify/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.home_view, name='hello'),  # URL for home view
+    path('hello/', views.home_view, name='hello'),  # URL for hello world view
+    path('signup/', views.signup_view, name='signup'),  # URL for signup view
+    path('login/', views.login_view, name='login'), # URL for login view
 ]
--- LoginSystem/Loginify/views.py
+++ LoginSystem/Loginify/views.py
@@ -1,6 +1,43 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import UserDetails
 
 # Create your views here.
 def home_view(request):
     return HttpResponse("Hello, world!")
+
+
+# View logic for signup
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        # Checking if email already exists
+        if UserDetails.objects.filter(email=email).exists():
+            return HttpResponse("Email already exists!")
+        else:
+            # Creating a new user and save the changes to DB
+            user = UserDetails(username=username, email=email, password=password)
+            user.save()
+            # Redirect to login page after creating a new user
+            return redirect('login')
+    return render(request, 'Loginify\signup.html')
+
+
+# View logic for login
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        # Check if the entered details are correct or not
+        try:
+            # if details are correct display the message with the username
+            user = UserDetails.objects.get(email=email, password=password)
+            return HttpResponse(f"Welcome to the application, {user.username}!")
+        except UserDetails.DoesNotExist:
+            # if details are incorrect throw error
+            return HttpResponse("Invalid email or password!")
+    return render(request, 'Loginify\login.html')
