d578a513ed4ed16e0244b8141b45e925b8029eee
malyaj93 <m.sirothia93@gmail.com>
2024-09-22 22:16:44 -0700

    Task 5 done
    1. Added functionalities in view for get user, update user, update user partially, delete user
    2. Added screenshots for all the newly added APIs

--- LoginSystem/Loginify/urls.py
+++ LoginSystem/Loginify/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('hello/', views.home_view, name='hello'),  # URL for hello world view
     path('signup/', views.signup_view, name='signup'),  # URL for signup view
     path('login/', views.login_view, name='login'), # URL for login view
+    path('get_all_users/', views.get_all_users, name='get_all_users'),  # URL to fetch all the users
+    path('get_user_by_email/<str:email>/', views.get_user_by_email, name='get_user_by_email'),  # URL to fetch user by email
+    path('update_user/<str:email>/', views.update_user, name='update_user'),    # URL to update user details
+    path('update_partial_user/<str:username>/', views.update_partial_user, name='update_partial_user'), # URL to update partial details of a user
+    path('delete_user/<str:username>/', views.delete_user, name='delete_user'), # URL to delete a user
 ]
--- LoginSystem/Loginify/views.py
+++ LoginSystem/Loginify/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
+import json
+
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserDetailsSerializer
+
 from .models import UserDetails
 
 # Create your views here.
@@ -41,3 +47,69 @@ def login_view(request):
             # if details are incorrect throw error
             return HttpResponse("Invalid email or password!")
     return render(request, 'Loginify\login.html')
+
+
+# View to get all user details
+@api_view(['GET'])
+def get_all_users(request):
+    if request.method == 'GET':
+        try:
+            all_users = UserDetails.objects.all()
+            serializer_data = UserDetailsSerializer(all_users, many=True)
+            return JsonResponse(serializer_data.data, safe=False)
+        except Exception as e:
+            return JsonResponse({"error": str(e)})
+
+
+# View to get a single user by email
+@api_view(['GET'])
+def get_user_by_email(request, email):
+    if request.method == 'GET':
+        try:
+            user_by_email = UserDetails.objects.get(email=email)
+            serializer_data = UserDetailsSerializer(user_by_email)
+            return JsonResponse(serializer_data.data, safe=False)
+        except UserDetails.DoesNotExist:
+                    return Response({'error': 'User not found'}, status=404)
+
+# View to update a user's details
+@api_view(['PUT'])
+def update_user(request, email):
+    if request.method == 'PUT':
+        try:
+            user_data = UserDetails.objects.get(email=email)
+            input_data= json.loads(request.body)
+            serializer_data = UserDetailsSerializer(user_data, data=input_data)
+            if serializer_data.is_valid():
+                serializer_data.save()
+                return JsonResponse({'message': 'User data updated successfully'}, status=200)
+            else:
+                return JsonResponse(serializer_data.errors, status=400)
+        except UserDetails.DoesNotExist:
+            return Response({'error': 'User not found'}, status=404)
+
+# View to update a partial user's details
+@api_view(['PATCH'])
+def update_partial_user(request, username):
+    if request.method == 'PATCH':
+        try:
+            user_data = UserDetails.objects.get(username=username)
+            input_data= json.loads(request.body)
+            serializer_data = UserDetailsSerializer(user_data, data=input_data, partial=True)
+            if serializer_data.is_valid():
+                serializer_data.save()
+                return JsonResponse({'message': 'User data updated successfully'}, status=200)
+            else:
+                return JsonResponse(serializer_data.errors, status=400)
+        except UserDetails.DoesNotExist:
+            return Response({'error': 'User not found'}, status=404)
+
+@api_view(['DELETE'])
+def delete_user(request, username):
+    if request.method == 'DELETE':
+        try:
+            user_data = UserDetails.objects.get(username=username)
+            user_data.delete()
+            return JsonResponse({'message': 'User deleted successfully'}, status=204)
+        except UserDetails.DoesNotExist:
+            return Response({'error': 'User not found'}, status=404)
